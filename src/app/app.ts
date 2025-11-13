c25ac47f7fc6ae89c7b6c62e09bfb7e1e211edaa
Derrick Similien <dsimili1@my.westga.edu>
2025-11-13 00:58:33 -0500

    Initial Angular project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header';
+import { HomeComponent } from './components/home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, HomeComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css'],
 })
 export class App {
   protected readonly title = signal('angular-practice');
