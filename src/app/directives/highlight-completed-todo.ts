607995c843bc1e300caa893d920e40cf4caa73f1
Derrick Similien <dsimili1@my.westga.edu>
2025-12-23 00:30:49 -0500

    Fix highlight completed todo directive

--- src/app/directives/highlight-completed-todo.ts
+++ src/app/directives/highlight-completed-todo.ts
@@ -1,10 +1,25 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, effect, inject, input } from '@angular/core';
 
 @Directive({
-  selector: '[appHighlightCompletedTodo]'
+  selector: '[appHighlightCompletedTodo]',
+  standalone: true,
 })
 export class HighlightCompletedTodo {
+  isCompleted = input(false);
 
-  constructor() { }
+  private el = inject(ElementRef<HTMLElement>);
 
+  stylesEffect = effect(() => {
+    const nativeEl = this.el.nativeElement;
+
+    if (this.isCompleted()) {
+      nativeEl.style.textDecoration = 'line-through';
+      nativeEl.style.backgroundColor = '#d3f9d8';
+      nativeEl.style.color = '#2f855a';
+    } else {
+      nativeEl.style.textDecoration = 'none';
+      nativeEl.style.backgroundColor = '#fff';
+      nativeEl.style.color = '#000';
+    }
+  });
 }
