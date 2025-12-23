93b0a0901066f50cb5c734c322566f8306341d3c
Derrick Similien <dsimili1@my.westga.edu>
2025-12-23 02:08:04 -0500

    Add standalone todo-item component with signal inputs/outputs and highlight directive

--- src/app/components/todo-item/todo-item.ts
+++ src/app/components/todo-item/todo-item.ts
@@ -1,12 +1,15 @@
-import { Component, input } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import type { Todo } from '../../model/todo.type';
+import { HighlightCompletedTodo } from '../../directives/highlight-completed-todo';
 
 @Component({
   selector: 'app-todo-item',
   standalone: true,
+  imports: [HighlightCompletedTodo],
   templateUrl: './todo-item.html',
   styleUrl: './todo-item.scss',
 })
 export class TodoItemComponent {
   todo = input.required<Todo>();
+  todoToggled = output<Todo>();
 }
